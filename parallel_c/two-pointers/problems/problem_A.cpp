f1560e68e5d76a566c037c5293c8bf9553430468
Denis Mironov <denisquantitativeresearcher@gmail.com>
2025-02-04 21:44:16 -0600

    Add problem_A task

---
+++ parallel_c/two-pointers/problems/problem_A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+using namespace std;
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int n, r;
+  cin >> n >> r;
+  
+  vector<int> d(n, 0);
+  for (int i = 0; i < n; ++i) {
+    cin >> d[i];
+  }
+  
+  int64_t ans = 0;
+  int right = 0;
+  for (int left = 0; left < n; ++left) {
+    while (right < n && d[right] - d[left] <= r) {
+      ++right;
+    }
+    ans += n - right;
+  }
+  cout << ans;
+  
+  
+  return 0;
+}
