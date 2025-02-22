e2facc8e21c690f74d6553895520bb4596bf51e5
Denis Mironov <denisquantitativeresearcher@gmail.com>
2025-02-22 11:25:41 -0600

    solved problem_M from binary search

---
+++ parallel_c/binary_search/problems/problem_M.cpp
@@ -0,0 +1,54 @@
+
+// Задача M: Велогонка
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+double f(double t, const vector<int>& x0, vector<int>& v0) {
+  double maximum = -1e20;
+  double minimum =  1e20;
+  int n = static_cast<int>(x0.size());
+  for (int i = 0; i < n; ++i) {
+    maximum = max(maximum, x0[i] + v0[i] * t);
+    minimum = min(minimum, x0[i] + v0[i] * t);
+  }
+  return maximum - minimum;
+}
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int n;
+  cin >> n;
+  
+  vector<int> x0(n);
+  vector<int> v0(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> x0[i];
+    cin >> v0[i];
+  }
+  
+  double left = 0;
+  double right = 1e10;
+  for (int i = 0; i < 200; ++i) {
+    double a = left + (right - left) / 3.0;
+    double b = left + 2 * (right - left) / 3.0;
+    if (f(b, x0, v0) > f(a, x0, v0)) {
+      right = b;
+    }
+    else {
+      left = a;
+    }
+  }
+  
+  cout << setprecision(20);
+  cout << left << " " << f(left, x0, v0);
+  
+  return 0;
+}
